e99eba005a815e999a316becf55b53dc8a190376
big sean <mseangod08@gmail.com>
2026-04-26 22:46:19 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,11 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("scoutnet-cache").then((cache) => {
-      return cache.addAll([
-        "home.html",
-        "icons/logo.png"
-      ]);
-    })
-  );
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", () => {
+  clients.claim();
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
